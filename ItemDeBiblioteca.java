b4aae408fa244f08fe024537b065cee44f8faff4
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-10-24 11:32:43 -0300

    Create ItemDeBiblioteca.java

---
+++ ItemDeBiblioteca.java
@@ -0,0 +1,15 @@
+public interface ItemDeBiblioteca {
+	int diasemprestimo = 7;
+	
+	public static void getEmprestado() {
+	}
+	
+	public static void getLocalizacao() {
+	}
+	
+	public static void Empresta() {
+	}
+	
+	public static void Devolve() {
+	}
+}
